26e130efb4337246ec1f02f2d724c30ecb81878e
Nidhi <anilgopalka@rediffmail.com>
2026-02-22 10:00:53 +0530

    Updated filter code

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import numpy as np
 def apply_filter(image,ftype):
     img=image.copy()
     if ftype=="red":
-        img[:,:,1]= img[:,:,2]=0
+        img[:,:,0]=img[:,:,1]=0
     elif ftype=="green":
         img[:,:,0] = img[:,:,2]=0
     elif ftype=="blue":
-        img[:,:,0]=img[:,:,1]=0
+        img[:,:,1]= img[:,:,2]=0
     elif ftype=="sobel":
         gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
         sx=cv2.Sobel(gray,cv2.CV_64F,1,0,ksize=3)
@@ -16,7 +16,7 @@ def apply_filter(image,ftype):
         img=cv2.cvtColor(sob,cv2.COLOR_GRAY2BGR)
     elif ftype=="canny":
         gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-        can=cv2.canny(gray,100,200)
+        can=cv2.Canny(gray,100,200)
         img=cv2.cvtColor(can,cv2.COLOR_GRAY2BGR)
     elif ftype=="cartoon":
         gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
